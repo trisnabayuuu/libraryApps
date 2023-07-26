225010894e11798f4f9166a7c97049fcc534b6b1
Trisna Bayu <btrisna90@gmail.com>
2023-07-27 01:05:41 +0700

    mencoba strapi

--- lib/books.dart
+++ lib/books.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class BooksDetail extends StatelessWidget {
   const BooksDetail({Key? key}) : super(key: key);
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart';
 
 import 'books.dart';
 
